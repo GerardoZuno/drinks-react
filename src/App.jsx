1aefd01f091e96a89c13d5ea4a0e72343163732a
GerardoZuno <gerardozunounitec@gmail.com>
2021-08-24 13:35:09 -0500

    Add Form Component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Form from './components/Form'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <div className="container mt-5">
+        <div className="row">
+          <Form />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <h1>
+            Formulario
+        </h1>
+    )
+}
+
+export default Form
